091757dbe0bfb5f19ae48b3bea5291331f52385f
KOOKIEWAR <giozach0000@gmail.com>
2024-06-13 22:01:53 +0200

    calcolatore velocità

---
+++ script.js
@@ -0,0 +1,87 @@
+function firstParameter(distance, unita){
+    switch(unita){
+        case 0: distance *= 1.0;
+        break;
+        case 1:  distance *= 1000.0;
+        break;
+        case 2:  distance *= 1609.0;
+        break;
+        case 3: distance /= 1.094;
+        break;
+    }
+
+    return distance;
+}
+
+function secondParameter(hours, minutes, seconds){
+    const time = (hours * 3600) + (minutes * 60)+ seconds;
+    return time;
+}
+
+function passo1(distance, time, num){
+    if(distance == 0) return 0;
+    else if (distance > 0) return (time / (distance / num)) / 60;
+}
+
+function passo2(passoMinuti){
+    
+    if((passoMinuti % 1) == 0) return 0;
+    else if((passoMinuti % 1) > 0){
+        return (passoMinuti % 1) * 60;
+    }
+}
+
+function unitOrari(distance, time, num){
+    var ore = time / 3600;
+    return (distance / num) / ore;
+}
+
+function metriPerSecondo(distance, time){
+    return distance / time;
+}
+
+function control(value){
+    if(value < 0) alert("Inserisci numeri positivi");
+}
+
+function calculate(){
+    const distanceInput = document.getElementById("distance");
+    const unitDistance = document.getElementById("unitDistance");
+    const hoursInput = document.getElementById("hours");
+    const minutesInput = document.getElementById("minutes");
+    const secondsInput = document.getElementById("seconds");
+    const speedKm1 = document.getElementById("speedKm1");
+    const speedKm2 = document.getElementById("speedKm2");
+    const distTime1 = document.getElementById("Km/h");
+    const distTime2 = document.getElementById("Metri/secondo");
+    const speedMiglio1 = document.getElementById("PassoMiglio1");
+    const speedMiglio2 = document.getElementById("PassoMiglio2");
+    const distTime3 = document.getElementById("Miglia/h");
+
+    var distance = parseFloat(distanceInput.value);
+    var hours = parseInt(hoursInput.value);
+    var minutes = parseInt(minutesInput.value);
+    var seconds = parseInt(secondsInput.value);
+    control(distance);
+    control(hours);
+    control(minutes);
+    control(seconds);
+
+    if(isNaN(distance) || isNaN(hours) || isNaN(minutes) || isNaN(seconds)) return;
+    else{
+        const dist = firstParameter(distance, unitDistance.selectedIndex);
+        const totalTime = secondParameter(hours, minutes, seconds);
+        
+        speedKm1.value = parseInt(passo1(dist, totalTime, 1000));
+        speedKm2.value = parseInt(passo2(passo1(dist, totalTime, 1000)));
+        distTime1.value = parseFloat(unitOrari(dist, totalTime, 1000));
+        distTime2.value = parseFloat(metriPerSecondo(dist, totalTime));
+        speedMiglio1.value = parseInt(passo1(dist, totalTime, 1609));
+        speedMiglio2.value = parseInt(passo2(passo1(dist, totalTime, 1609)));
+        distTime3.value = parseFloat(unitOrari(dist, totalTime, 1609));
+    }
+
+    //const dist = firstParameter(distance, unitDistance.selectedIndex);
+    //const totalTime = secondParameter(hours, minutes, seconds);
+    //alert(dist);
+}
