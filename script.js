92aa5cabb9bb58504250055f74ec16f107ce9b15
KOOKIEWAR <giozach0000@gmail.com>
2024-06-14 11:40:01 +0200

    aggiunta passaggio

--- script.js
+++ script.js
@@ -40,10 +40,6 @@ function metriPerSecondo(distance, time){
     return distance / time;
 }
 
-function control(value){
-    if(value < 0) alert("Inserisci numeri positivi");
-}
-
 function calculate(){
     const distanceInput = document.getElementById("distance");
     const unitDistance = document.getElementById("unitDistance");
@@ -62,12 +58,9 @@ function calculate(){
     var hours = parseInt(hoursInput.value);
     var minutes = parseInt(minutesInput.value);
     var seconds = parseInt(secondsInput.value);
-    control(distance);
-    control(hours);
-    control(minutes);
-    control(seconds);
 
-    if(isNaN(distance) || isNaN(hours) || isNaN(minutes) || isNaN(seconds)) return;
+    if(isNaN(distance) || isNaN(hours) || isNaN(minutes) || isNaN(seconds) ||
+    distance<0 || hours < 0 || minutes < 0 || seconds < 0) alert("Non possono esserci valori vuoti!!!");
     else{
         const dist = firstParameter(distance, unitDistance.selectedIndex);
         const totalTime = secondParameter(hours, minutes, seconds);
@@ -79,9 +72,34 @@ function calculate(){
         speedMiglio1.value = parseInt(passo1(dist, totalTime, 1609));
         speedMiglio2.value = parseInt(passo2(passo1(dist, totalTime, 1609)));
         distTime3.value = parseFloat(unitOrari(dist, totalTime, 1609));
+        passoXMetri(dist, totalTime);
     }
 
     //const dist = firstParameter(distance, unitDistance.selectedIndex);
     //const totalTime = secondParameter(hours, minutes, seconds);
     //alert(dist);
+}
+
+function passoXMetri(distance, time){
+    const passaggioInput = document.getElementById("passaggio");
+    const unitPassaggio = document.getElementById("unitPassaggio");
+    const minutiPass = document.getElementById("passaggioResult1");
+    const secondiPass = document.getElementById("passaggioResult2");
+
+    var passaggio = parseInt(passaggioInput.value);
+
+    var pass = firstParameter(passaggio, unitPassaggio.selectedIndex);
+
+    if(passaggio < 0 || isNaN(passaggio) || pass > 10000) alert("Se non vuoi sapere nessun passaggio, inserisci 0 \n(MAX passaggio 10km)");
+    else{
+        var x = distance / pass;
+        var y = time / x;
+        document.getElementById("fraseMetri").innerHTML = "Passaggio a " + pass;
+
+        if(distance == 0) minutiPass.value = 0;
+        else if (distance > 0) minutiPass.value = parseInt(y / 60);
+
+        if(((y/60) % 1) == 0) secondiPass.value = 0;
+        else if(((y/60) % 1) > 0) secondiPass.value = parseInt(((y/60) % 1) * 60);
+    }
 }
